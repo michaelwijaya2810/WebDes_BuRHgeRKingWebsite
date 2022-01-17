8e7607ef6c3cd38d8ef928b57b9717b96bc584e8
michaelwijaya2810 <michaelwijaya.mw20@gmail.com>
2022-01-18 00:25:14 +0700

    commitChanges

--- JS/imageSlider.js
+++ JS/imageSlider.js
@@ -1,36 +1,25 @@
-'user strict';
-
-$(function() {
-
+$(document).ready(function(){
     var width = 720;
     var animationSpeed = 1000;
     var pause = 1000;
     var currSlide = 1;
 
-    var $slider = $('#slider');
-    var $slideContainer = $('.slides', $slider);
-    var $slides = $('.slide', $slider);
+    var $sliderOuter = $('.sliderOuter');
+    var $slideInner = $('.sliderInner', $sliderOuter);
+    var $slides = $('.slide', $sliderOuter);
 
     var interval;
 
-    function startSlider() {
+    function startSlider(){
         interval = setInterval(function(){
-            $slideContainer.animate({'margin-left': '-='+width }, animationSpeed, function(){
-                currSlide++;
-                if(currSlide == $slides.length){
-                    currentSlide = 1;
-                    $slideContainer.css('margin-left', 0);
+            $slideInner.animate({
+                'margin-left': '-='+width }, animationSpeed, function(){
+                if(++currSlide === $slides.length){
+                    currSlide = 1;
+                    $slideInner.css('margin-left', 0);
                 }
             });
         }, pause);
     }
-
-    function stopSlider(){
-        clearInterval(interval);
-    }
-    
-
-    $slideContainer.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
-
     startSlider();
 });
