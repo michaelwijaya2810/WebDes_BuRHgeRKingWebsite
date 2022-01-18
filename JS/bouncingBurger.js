d43a638879f330c4c70697d08394345af5c47734
michaelwijaya2810 <michaelwijaya.mw20@gmail.com>
2022-01-18 07:44:38 +0700

    finalCommit

--- JS/bouncingBurger.js
+++ JS/bouncingBurger.js
@@ -1,59 +1,42 @@
-var canvas = document.getElementById('burgerCanvas');
-var context = canvas.getContext('2d');
+var canvas = document.getElementById('burgerCanvas')
+var context = canvas.getContext('2d')
 
-canvas.width = canvas.scrollWidth;
-canvas.height = canvas.scrollHeight;
+canvas.width = canvas.scrollWidth
+canvas.height = canvas.scrollHeight
 
-var cHeight = canvas.width;
-var cHeight = canvas.height;
-var img = document.getElementById('burgerCanvasimg');
-var x = 0, y = 0, r=50;
-var speedx =3;
-var speedy =3;
+var cHeight = canvas.width
+var cHeight = canvas.height
+var img = document.getElementById('burgerCanvasimg')
+var x = 0, y = 0, r=50
+var speedx =3
+var speedy =3
 
-img.style.width = "100px";
-console.log(canvas.height -100);
+img.style.width = "100px"
+
 function bounceAnim(){
-
-
-    
-
     window.requestAnimationFrame(draw)
 }
 
 function draw()
 {
-    context.drawImage(img, x, y,100,100);
+    context.drawImage(img, x, y,120,120)
 
-    
-    x+=speedx;
-    y+=speedy;
+    x+=speedx
+    y+=speedy
 
-    if(x >= canvas.width - 100)
-    {
-        speedx *= -1;
+    if(x >= canvas.width - 100){
+        speedx *= -1
     }
-    if(x == 0)
-    {
-        speedx*= -1;
+    if(x == 0){
+        speedx*= -1
     }
-    if(y==0)
-    {
-        speedy *= -1;
+    if(y==0){
+        speedy *= -1
     }
-    if(y >= canvas.height -100)
-    {
+    if(y >= canvas.height -100){
         speedy *=-1
     }
-    
-    // context.moveTo(100,100);
-    // context.lineTo(200,100);
-    // context.strokeStyle = '#f0000'
-    // context.stroke();
     window.requestAnimationFrame(draw)
 
 }
-
-// setInterval(bounceAnim, 10);
-
-bounceAnim();
+bounceAnim()
--- JS/imageSlider.js
+++ JS/imageSlider.js
@@ -1,25 +1,17 @@
 $(document).ready(function(){
-    var width = 720;
-    var animationSpeed = 1000;
-    var pause = 1000;
-    var currSlide = 1;
+    setInterval(() =>{
+        let contWidth = $('.sliderOuter').width()
+        let left = $('.sliderInner').css('left').split('px')[0]
 
-    var $sliderOuter = $('.sliderOuter');
-    var $slideInner = $('.sliderInner', $sliderOuter);
-    var $slides = $('.slide', $sliderOuter);
-
-    var interval;
-
-    function startSlider(){
-        interval = setInterval(function(){
-            $slideInner.animate({
-                'margin-left': '-='+width }, animationSpeed, function(){
-                if(++currSlide === $slides.length){
-                    currSlide = 1;
-                    $slideInner.css('margin-left', 0);
-                }
-            });
-        }, pause);
-    }
-    startSlider();
+        let imgCount = 3
+        if(left == contWidth*-1*(imgCount-1)){
+            $('.sliderInner').animate({
+                left: 0
+            })
+        }else{
+            $('.sliderInner').animate({
+                left: left-contWidth
+            })
+        }
+    },5000)
 });
